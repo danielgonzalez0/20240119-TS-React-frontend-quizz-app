81e32f4349d4c63f00e0f1524c88b6dcda507dc2
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-02-07 17:38:37 +0100

    fix url background to vercel by adding images on folder public

--- src/styles/globalSettings.ts
+++ src/styles/globalSettings.ts
@@ -1,65 +1,59 @@
-import { createGlobalStyle } from 'styled-components';
-import lightBackgroundDesktop from '../assets/images/pattern-background-desktop-light.svg';
-import darkBackgroundDesktop from '../assets/images/pattern-background-desktop-dark.svg';
-import lightBackgroundTablet from '../assets/images/pattern-background-tablet-light.svg';
-import darkBackgroundTablet from '../assets/images/pattern-background-tablet-dark.svg';
-import lightBackgroundMobile from '../assets/images/pattern-background-mobile-light.svg';
-import darkBackgroundMobile from '../assets/images/pattern-background-mobile-dark.svg';
+import { createGlobalStyle } from "styled-components";
 
 export const theme = {
   size: {
-    smallmobile: '375px',
-    mobile: '768px',
-    tablet: '1024px',
+    smallmobile: "375px",
+    mobile: "768px",
+    tablet: "1024px",
   },
 
   colors: {
-    primary: '#A729F5',
-    secondary: '#313E51',
-    tertiary: '#3B4D66',
-    quaternary: '#626c7f',
-    quinary: '#abc1e1',
-    senary: '#f4f6fa',
-    septenary: '#ffffff',
-    octonary: '#26d782',
-    nonary: '#ee5454',
+    primary: "#A729F5",
+    secondary: "#313E51",
+    tertiary: "#3B4D66",
+    quaternary: "#626c7f",
+    quinary: "#abc1e1",
+    senary: "#f4f6fa",
+    septenary: "#ffffff",
+    octonary: "#26d782",
+    nonary: "#ee5454",
     // Ajoutez d'autres couleurs ici
   },
   typography: {
     display: {
-      fontFamily: 'Rubik-Medium, sans-serif',
-      fontSize: '144px',
-      lineHeight: '100%',
+      fontFamily: "Rubik-Medium, sans-serif",
+      fontSize: "144px",
+      lineHeight: "100%",
     },
     headingL: {
-      fontFamily: 'Rubik-Medium, sans-serif',
-      fontSize: '64px',
-      lineHeight: '100%',
+      fontFamily: "Rubik-Medium, sans-serif",
+      fontSize: "64px",
+      lineHeight: "100%",
     },
     headingLRegular: {
-      fontFamily: 'Rubik-Regular, sans-serif',
-      fontSize: '64px',
-      lineHeight: '100%',
+      fontFamily: "Rubik-Regular, sans-serif",
+      fontSize: "64px",
+      lineHeight: "100%",
     },
     headingM: {
-      fontFamily: 'Rubik-Medium, sans-serif',
-      fontSize: '36px',
-      lineHeight: '100%',
+      fontFamily: "Rubik-Medium, sans-serif",
+      fontSize: "36px",
+      lineHeight: "100%",
     },
     headingS: {
-      fontFamily: 'Rubik-Medium, sans-serif',
-      fontSize: '28px',
-      lineHeight: '100%',
+      fontFamily: "Rubik-Medium, sans-serif",
+      fontSize: "28px",
+      lineHeight: "100%",
     },
     bodyM: {
-      fontFamily: 'Rubik-Regular, sans-serif',
-      fontSize: '12px',
-      lineHeight: '150%',
+      fontFamily: "Rubik-Regular, sans-serif",
+      fontSize: "12px",
+      lineHeight: "150%",
     },
     bodyS: {
-      fontFamily: 'Rubik-Italic, sans-serif',
-      fontSize: '12px',
-      lineHeight: '150%',
+      fontFamily: "Rubik-Italic, sans-serif",
+      fontSize: "12px",
+      lineHeight: "150%",
     },
   },
 };
@@ -99,8 +93,8 @@ export const GlobalStyle = createGlobalStyle<GlobalStyleProps>`
 
     background: ${({ darkMode }) =>
       darkMode
-        ? `url(${darkBackgroundDesktop})`
-        : `url(${lightBackgroundDesktop})`} 
+        ? 'url("/images/pattern-background-desktop-dark.svg")'
+        : 'url("/images/pattern-background-desktop-light.svg")'} 
       no-repeat center center fixed;
     background-size: cover;
     background-color: ${({ theme, darkMode }) =>
@@ -115,8 +109,8 @@ export const GlobalStyle = createGlobalStyle<GlobalStyleProps>`
     @media screen and (max-width: ${({ theme }) => theme.size.tablet}){
      background: ${({ darkMode }) =>
        darkMode
-         ? `url(${darkBackgroundTablet})`
-         : `url(${lightBackgroundTablet})`}
+         ? 'url("/images/pattern-background-tablet-dark.svg")'
+         : 'url("/images/pattern-background-tablet-light.svg")'}
            no-repeat top -100px left fixed;
       background-size: 55% 65%;
       background-color: ${({ theme, darkMode }) =>
@@ -126,8 +120,8 @@ export const GlobalStyle = createGlobalStyle<GlobalStyleProps>`
     @media screen and (max-width: ${({ theme }) => theme.size.mobile}){
       background: ${({ darkMode }) =>
         darkMode
-          ? `url(${darkBackgroundMobile})`
-          : `url(${lightBackgroundMobile})`}
+          ? 'url("/images/pattern-background-mobile-dark.svg")'
+          : 'url("/images/pattern-background-mobile-light.svg")'}
              no-repeat top left fixed;
       background-color: ${({ theme, darkMode }) =>
         darkMode ? theme.colors.secondary : theme.colors.senary};
