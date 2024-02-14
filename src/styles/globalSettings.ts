a7535798d41b23e44489f603ca98822604693d13
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-02-14 10:09:14 +0100

    save init version

--- src/styles/globalSettings.ts
+++ src/styles/globalSettings.ts
@@ -1,59 +1,59 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 export const theme = {
   size: {
-    smallmobile: "375px",
-    mobile: "768px",
-    tablet: "1024px",
+    smallmobile: '375px',
+    mobile: '768px',
+    tablet: '1024px',
   },
 
   colors: {
-    primary: "#A729F5",
-    secondary: "#313E51",
-    tertiary: "#3B4D66",
-    quaternary: "#626c7f",
-    quinary: "#abc1e1",
-    senary: "#f4f6fa",
-    septenary: "#ffffff",
-    octonary: "#26d782",
-    nonary: "#ee5454",
+    primary: '#A729F5',
+    secondary: '#313E51',
+    tertiary: '#3B4D66',
+    quaternary: '#626c7f',
+    quinary: '#abc1e1',
+    senary: '#f4f6fa',
+    septenary: '#ffffff',
+    octonary: '#26d782',
+    nonary: '#ee5454',
     // Ajoutez d'autres couleurs ici
   },
   typography: {
     display: {
-      fontFamily: "Rubik-Medium, sans-serif",
-      fontSize: "144px",
-      lineHeight: "100%",
+      fontFamily: 'Rubik-Medium, sans-serif',
+      fontSize: '144px',
+      lineHeight: '100%',
     },
     headingL: {
-      fontFamily: "Rubik-Medium, sans-serif",
-      fontSize: "64px",
-      lineHeight: "100%",
+      fontFamily: 'Rubik-Medium, sans-serif',
+      fontSize: '64px',
+      lineHeight: '100%',
     },
     headingLRegular: {
-      fontFamily: "Rubik-Regular, sans-serif",
-      fontSize: "64px",
-      lineHeight: "100%",
+      fontFamily: 'Rubik-Regular, sans-serif',
+      fontSize: '64px',
+      lineHeight: '100%',
     },
     headingM: {
-      fontFamily: "Rubik-Medium, sans-serif",
-      fontSize: "36px",
-      lineHeight: "100%",
+      fontFamily: 'Rubik-Medium, sans-serif',
+      fontSize: '36px',
+      lineHeight: '100%',
     },
     headingS: {
-      fontFamily: "Rubik-Medium, sans-serif",
-      fontSize: "28px",
-      lineHeight: "100%",
+      fontFamily: 'Rubik-Medium, sans-serif',
+      fontSize: '28px',
+      lineHeight: '100%',
     },
     bodyM: {
-      fontFamily: "Rubik-Regular, sans-serif",
-      fontSize: "12px",
-      lineHeight: "150%",
+      fontFamily: 'Rubik-Regular, sans-serif',
+      fontSize: '12px',
+      lineHeight: '150%',
     },
     bodyS: {
-      fontFamily: "Rubik-Italic, sans-serif",
-      fontSize: "12px",
-      lineHeight: "150%",
+      fontFamily: 'Rubik-Italic, sans-serif',
+      fontSize: '12px',
+      lineHeight: '150%',
     },
   },
 };
@@ -66,19 +66,19 @@ export const GlobalStyle = createGlobalStyle<GlobalStyleProps>`
 
 @font-face {
   font-family: Rubik-Regular;
-  src: url('/src/assets/fonts/static/Rubik-Regular.ttf');
+  src: url('/fonts/Rubik-Regular.ttf');
   font-weight: normal;
   font-style: normal;
 }
 @font-face {
   font-family: Rubik-Italic;
-  src: url('/src/assets/fonts/static/Rubik-Italic.ttf');
+  src: url('/fonts/Rubik-Italic.ttf');
   font-weight: normal;
   font-style: italic;
 }
 @font-face {
   font-family: Rubik-Medium;
-  src: url('src/assets/fonts/static/Rubik-Medium.ttf');
+  src: url('/fonts/Rubik-Medium.ttf');
   font-weight: bold;
   font-style: normal;
 }
